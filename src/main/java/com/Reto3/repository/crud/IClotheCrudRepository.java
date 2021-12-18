e0eb16249f56a0d3de50645be026d0c84df5ed29
AFelipeGomez <andrex930@gmail.com>
2021-12-18 13:12:44 -0500

    reto3

---
+++ src/main/java/com/Reto3/repository/crud/IClotheCrudRepository.java
@@ -0,0 +1,12 @@
+package com.Reto3.repository.crud;
+
+import com.Reto3.model.Clothe;
+import org.springframework.data.repository.CrudRepository;
+
+
+
+
+
+public interface IClotheCrudRepository extends CrudRepository<Clothe, String> {
+
+}
